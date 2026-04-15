699ca0f0bb2778d48a047c037bb204eacd6e0436
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-14 22:19:13 -0300

    Commit inicial: configuração do projeto com Spring Boot
    Initial commit: project setup with Spring Boot

---
+++ src/main/java/com/micaela/internal_ai_chat/InternalAiChatApplication.java
@@ -0,0 +1,13 @@
+package com.micaela.internal_ai_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalAiChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalAiChatApplication.class, args);
+	}
+
+}
