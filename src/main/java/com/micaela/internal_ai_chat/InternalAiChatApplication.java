2177d7438f306d038953bcb7d41fbc817e62bcbf
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-17 22:27:42 -0300

    feat: adicionando OpenFeign e configurando Groq API
    feat: adding OpenFeign and configuring Groq API

--- src/main/java/com/micaela/internal_ai_chat/InternalAiChatApplication.java
+++ src/main/java/com/micaela/internal_ai_chat/InternalAiChatApplication.java
@@ -2,12 +2,12 @@ package com.micaela.internal_ai_chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InternalAiChatApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InternalAiChatApplication.class, args);
 	}
-
 }
