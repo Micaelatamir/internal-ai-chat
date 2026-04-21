7d28635131f1716a862dc0b78991acae0f516a5e
Micaela Tamires <micaela.tamir@gmail.com>
2026-04-20 23:08:59 -0300

    feat:  criando GroqClient com Feign
    feat: Creating GroqClient with Feign

--- src/main/java/com/micaela/internal_ai_chat/client/GroqClient.java
+++ src/main/java/com/micaela/internal_ai_chat/client/GroqClient.java
@@ -1,4 +1,25 @@
 package com.micaela.internal_ai_chat.client;
 
-public class GroqClient {
+import com.micaela.internal_ai_chat.dto.request.GroqRequestDTO;
+import com.micaela.internal_ai_chat.dto.response.GroqResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "groq", url = "${groq.api.url}")
+public interface GroqClient {
+
+    @PostMapping
+    GroqResponseDTO chat(
+            @RequestHeader("Authorization") String authorization
+            // completa aqui!
+    );
+
+    @PostMapping
+    GroqResponseDTO chat(
+            @RequestHeader("Authorization") String authorization,
+            @RequestBody GroqRequestDTO request
+    );
+
 }
